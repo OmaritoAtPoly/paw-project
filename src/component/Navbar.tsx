c6086600281a741f7b094a5046b4b938b4836472
Omar Estrada Diaz <omar.diaz@musala.com>
2022-12-08 15:25:56 +0200

    finishing navbar with mobile left lateral panel

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,17 +1,25 @@
-import React from 'react';
-import {AiOutlineMenu, AiOutlineSearch} from 'react-icons/ai';
+import React, {useState} from 'react';
+import {AiOutlineMenu, AiOutlineSearch, AiOutlineClose} from 'react-icons/ai';
 import {BsFillCartFill} from 'react-icons/bs';
+import {TbTruckDelivery} from 'react-icons/tb';
+import {MdFavorite} from 'react-icons/md';
 
 const Navbar = () => {
+	const [navState, setNavState] = useState(false);
+
+	const handleNavState = () => {
+		setNavState(prev => !prev);
+	};
+
 	return (
-		<div className='border border-red-200 border-8 mx-auto flex justify-between items-center p-4'>
+		<div className='border-red-200 border-8 mx-auto flex justify-between items-center p-4'>
 			{/* left side */}
 			<div className='flex items-center'>
 				<div className='cursor-pointer'>
-					<AiOutlineMenu size={30} />
+					<AiOutlineMenu size={30} onClick={handleNavState} />
 				</div>
-				<h1 className='text-2xl sm:text-3xl lg:text-4xl px-2 '>
-					Best <span className='font-bold'>Eats</span>
+				<h1 className='text-2xl sm:text-3xl lg:text-4xl px-2 select-none'>
+					Best <span className='font-bold'>Eat</span>
 				</h1>
 
 				<div className='hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]'>
@@ -27,8 +35,22 @@ const Navbar = () => {
 			</div>
 			{/* cart button */}
 			<button type='submit' className='bg-black text-white hidden md:flex items-center p-2 rounded-2xl'>
-				<BsFillCartFill size={20} className='mr-2'/> Cart
+				<BsFillCartFill size={20} className='mr-2' /> Cart
 			</button>
+			{/* mobile menu */}
+			{/* overlay */}
+			{navState ? <div className='bg-black/70 fixed w-full h-screen left-0 top-0' onClick={handleNavState} onKeyUp={handleNavState} role='button' tabIndex={0} aria-label='hidden text' /> : ''}
+			{/* side drawer menu */}
+			<div className={`${navState ? 'fixed w-[300px] h-screen duration-300 bg-white left-0 top-0' : 'fixed w-[300px] h-screen duration-300 bg-white left-[-100%] top-0'} `}>
+				<AiOutlineClose size={25} className="absolute right-4 top-4 cursor-pointer" onClick={handleNavState} />
+				<h2 className='text-2xl p-4'>Best <span className='font-bold'>Eat</span></h2>
+				<nav>
+					<ul className='flex flex-col p-4'>
+						<li className='text-xl py-4 flex'><TbTruckDelivery size={25} className='mr-4' />Orders</li>
+						<li className='text-xl py-4 flex'><MdFavorite size={25} className='mr-4' />Favorites</li>
+					</ul>
+				</nav>
+			</div>
 		</div>
 	);
 };
