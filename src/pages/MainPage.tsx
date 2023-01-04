06d7fd3b4cb5d76c4ee93c343c519887f0947beb
Omar Estrada Diaz <omar.diaz@musala.com>
2023-01-04 15:34:36 +0200

    with a main NavBar layout ready

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -2,17 +2,13 @@ import React from 'react';
 import Category from '../component/Category';
 import HeadLinesCards from '../component/HeadLinesCards';
 import Hero from '../component/Hero';
-import Navbar from '../component/Navbar';
-import {RightDrawer} from '../component/RightDrawer';
 import Pets from '../component/Pets';
 
 export const MainPage = () => (
 	<>
-		<Navbar />
 		<Hero />
 		<HeadLinesCards />
 		<Pets />
 		<Category />
-		<RightDrawer />
 	</>
 );
--- src/pages/routes/index.tsx
+++ src/pages/routes/index.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import {Route, createBrowserRouter, createRoutesFromElements} from 'react-router-dom';
 import {ErrorPage} from '../ErrorPage';
 import {MainPage} from '../MainPage';
+import {MainPageLayout} from '../MainPageLayout';
 import {NotFoundPage} from '../NotFoundPage';
 import {PrivateRoutes} from '../PrivateRoutes';
 import {Register, userLoader} from '../Register';
 
 export const routerElements = createBrowserRouter(
 	createRoutesFromElements(
-		<Route errorElement={<ErrorPage />}>
+		<Route path='/' element={<MainPageLayout />} errorElement={<ErrorPage />}>
 			<Route index element={<MainPage />} />
 			<Route path='register' element={<PrivateRoutes />} >
 				<Route index element={<Register />} loader={userLoader} />
