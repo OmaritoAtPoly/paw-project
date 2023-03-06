77480e02ce6baf7381295d0556bd2bddfb8d67aa
omar.diaz@musala.com <omar.diaz@musala.com>
2023-03-06 13:56:53 +0200

    adding a fn to delete a pet by its id

--- src/pages/PetsPage.tsx
+++ src/pages/PetsPage.tsx
@@ -1,15 +1,23 @@
 import React, {useCallback, useEffect, useState} from 'react';
 import {Pet} from '../component/PetDetails/Pet';
 import {useGetAllApiCalls} from '../utils/apiCalls/useGetAllApiCalls';
+import {usePetAllApiCalls} from '../utils/apiCalls/usePutAllApiCalls';
 
 export const Pets = () => {
 	const [pets, setPets] = useState<Components.Schemas.Pet[]>();
 	const {getAllPets} = useGetAllApiCalls();
 
+	const {deletePet} = usePetAllApiCalls();
+
 	const handleGetAllPets = useCallback(async () => {
 		const value = await getAllPets();
 		if (value) setPets(value);
 	}, [getAllPets]);
+
+	const handleDeletePet = useCallback((petId: string) => async () => {
+		const value = await deletePet(petId);
+		if (value && (value.status >= 200 || value.status <= 299)) await handleGetAllPets();
+	}, [deletePet, handleGetAllPets]);
 
 	useEffect(() => {
 		void handleGetAllPets();
@@ -18,7 +26,7 @@ export const Pets = () => {
 	return (
 
 		<div>
-			{(pets && pets.length > 0) ? pets.map((a) => <Pet name={a.name} id={a.id} key={a.id} />) : <div>no hay</div>}
+			{(pets && pets.length > 0) ? pets.map((a) => <Pet name={a.name} id={a.id} key={a.id} deletePet={handleDeletePet} />) : <div>no hay</div>}
 		</div>
 
 	);
