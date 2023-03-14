fdc93b516dab6d58b85f453948f9cb64c9ec8eae
omar.diaz@musala.com <omar.diaz@musala.com>
2023-03-14 15:44:03 +0200

    adding a eye in the input for the keyword

--- src/utils/apiCalls/userApiCalls/register.ts
+++ src/utils/apiCalls/userApiCalls/register.ts
@@ -1,17 +1,14 @@
 import {type AxiosResponse} from 'axios';
 import {useCallback} from 'react';
-import {extendedDefaultUser} from '../../../data/data';
 import {axiosPublic} from '../../axiosConfiguration';
 
 export const useUserApiCalls = () => {
 
 	const register = useCallback(async (userData: Components.Schemas.UserDto) => {
-		let value: Components.Schemas.UserDto = extendedDefaultUser;
 		try {
 			const response: AxiosResponse<Components.Schemas.UserDto> = await axiosPublic.post('/v1/users', {...userData});
 			if (response.status >= 200 && response.status <= 299) {
-				value = response.data;
-				return value;
+				return true;
 			}
 		} catch (error) {
 			if (error instanceof Error) {
@@ -19,7 +16,7 @@ export const useUserApiCalls = () => {
 			}
 		}
 
-		return value;
+		return false;
 	}, []);
 
 	return {register};
