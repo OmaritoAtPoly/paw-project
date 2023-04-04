496892523495fac594c3999a45e06605028f1a2f
omar.diaz@musala.com <omar.diaz@musala.com>
2023-04-04 16:48:11 +0300

    qqqq

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
 
-export const ErrorPage = () => (<NavLink to='/'>there is an error</NavLink>);
+export const ErrorPage = () => (<NavLink to='/'>there is an erroeeeeeeer</NavLink>);
